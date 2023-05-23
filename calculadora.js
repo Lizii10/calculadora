eeeb8764b4e4e88e411f7724dc1d2fc934fc26b0
Lizii10 <lizvilchezp@gmail.com>
2023-05-22 23:57:18 -0300

    mi primer commit

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+let sumar = require("./sumar")
+let restar = require("./restar")
+let multiplicar = require("./multiplicar")
+let dividir = require("./dividir")
+
+console.log(restar(10,25))
+console.log(sumar(8,4))
+
+console.log(multiplicar(8,3))
+console.log(multiplicar(7,0))
+
+console.log(dividir(19,2))
+console.log(dividir(6,0))
+
+
